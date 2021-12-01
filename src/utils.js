c3722d9448208aca9c23e210932e86fcb33fb884
Andrew Semenov <exe312@gmail.com>
2021-12-02 00:11:13 +0200

    fixed linter errors

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,11 @@
 export const flipACoin = () => {
-    const side = Math.floor((Math.random() * 100));
-    if(side >= 50)
+  const side = Math.floor((Math.random() * 100));
+  if(side >= 50) {
     return true;
-    else
+  } else {
     return false;
   }
-  
-  export const isChecked = () => {
-    let checked = false;
-    if(flipACoin) {
-      checked = true;
-    } else {
-      checked = false;
-    }
-  }
-  export const getRandomInt = (min,max) => {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-  
+};
+
+export const getRandomInt = (min,max) =>
+  min + Math.floor(Math.random() * (max - min + 1));
